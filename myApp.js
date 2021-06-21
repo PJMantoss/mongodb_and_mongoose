4049b1d3c3a10deb813d0eb201dfc6491ec144a7
PJMantoss <joelptoss@gmail.com>
2021-06-21 14:18:34 +0100

    update myApp

--- myApp.js
+++ myApp.js
@@ -1,5 +1,4 @@
-require('mongoose');
-
-//mongodb+srv://Joel2:<password>@cluster0.sffs5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+require('dotenv').config();
+var mongoose = require('mongoose');
 
 mongoose.connect("mongodb+srv://Joel2:<kingofglory>@cluster0.sffs5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
