a5e9e21f2ef70e57f8ff1407d8e36f2afa5b9844
PJMantoss <joelptoss@gmail.com>
2021-06-21 14:19:40 +0100

    update myApp

--- myApp.js
+++ myApp.js
@@ -2,3 +2,6 @@ require('dotenv').config();
 var mongoose = require('mongoose');
 
 mongoose.connect("mongodb+srv://Joel2:kingofglory@cluster0.sffs5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
+
+//Challenge 2 - Create a Model
+const Schema = mongoose.Schema;
