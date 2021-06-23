96557e0e2d84215990bd77eceb983875dc8e9bac
PJMantoss <joelptoss@gmail.com>
2021-06-23 14:50:12 +0100

    update myApp

--- myApp.js
+++ myApp.js
@@ -112,6 +112,7 @@ const createManyPeople = (arrayOfPeople, done) => {
     })
   }
 
+  //Challenge 9 - Perform New Updates on a Document Using model.findOneAndUpdate()
   const findAndUpdate = (personName, done) => {
     const ageToSet = 20;
 
